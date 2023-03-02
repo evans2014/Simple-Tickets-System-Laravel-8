642ef895bf6975520485af7df3ad8b62d0037b97
Ivan Vasilev <i.vass@abv.bg>
2023-03-02 11:24:33 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,9 +11,9 @@
 
                     <p>You are logged in!</p>
 
-                    @if(Auth::user()->is_admin)
-                        @if ($tickets->isEmpty() )
 
+                        @if ($tickets->isEmpty() )
+                        <p>There are currently no tickets.</p>
                         @else
                             <div class="panel-heading">
                                 <h3>Tickets</h3>
@@ -50,11 +50,12 @@
                                         <td>
                                             @if($ticket->status === 'Open')
                                                 <a href="{{ url('tickets/' . $ticket->ticket_id) }}" class="btn btn-primary">Comment</a>
-
+                                                @if(Auth::user()->is_admin)
                                                 <form style="float:right;" action="{{ url('admin/close_ticket/' . $ticket->ticket_id) }}" method="POST">
                                                     {!! csrf_field() !!}
                                                     <button type="submit" class="btn btn-danger">Close</button>
                                                 </form>
+                                                @endif
                                             @endif
                                         </td>
                                     </tr>
@@ -64,74 +65,13 @@
 
                             {{ $tickets->render() }}
                         @endif
-                    @else
 
-                    @endif
 
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-
-
-                <div class="panel-body">
-                    @if ($ticket_user->isEmpty() )
-
-                    @else
-                        <div class="panel-heading">
-                            <h3>Tickets</h3>
-                        </div>
-                        <table class="table table-striped">
-                            <thead class="table-primary">
-                            <tr>
-                                <th>Category</th>
-                                <th>Title</th>
-                                <th>Status</th>
-                                <th>Last Updated</th>
-                                <th style="text-align:left" colspan="2">Actions</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach ($ticket_user as $ticket)
-                                <tr>
-                                    <td>
-                                        {{ $ticket->category->name }}
-                                    </td>
-                                    <td>
-                                        <a href="{{ url('tickets/'. $ticket->ticket_id) }}">
-                                             {{ $ticket->title }}
-                                        </a>
-                                    </td>
-                                    <td>
-                                        @if ($ticket->status === 'Open')
-                                            <span class="label label-success">{{ $ticket->status }}</span>
-                                        @else
-                                            <span class="label label-danger">{{ $ticket->status }}</span>
-                                        @endif
-                                    </td>
-                                    <td>{{ $ticket->updated_at }}</td>
-                                    <td>
-                                        @if($ticket->status === 'Open')
-                                            <a href="{{ url('tickets/' . $ticket->ticket_id) }}" class="btn btn-primary">Comment</a>
-                                            
-                                        @endif
-                                    </td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-
-                        {{ $ticket_user->render() }}
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
-
-</div>
+   </div>
 
 @endsection
